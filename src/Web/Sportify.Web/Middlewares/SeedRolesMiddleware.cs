c607b02950bd36fcaf1da94ddb618d591bef387b
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-02 11:12:32 +0200

    Added middleware to seed roles in database.

---
+++ src/Web/Sportify.Web/Middlewares/SeedRolesMiddleware.cs
@@ -0,0 +1,40 @@
+﻿namespace Sportify.Web.Middlewares
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Data;
+    using Data.Models;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class SeedRolesMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public SeedRolesMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, IServiceProvider serviceProvider, UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            var dbContext = serviceProvider.GetService<SportifyDbContext>();
+
+            if (!dbContext.Roles.Any())
+            {
+                await this.SeedRoles(userManager, roleManager);
+            }
+
+            await this.next(context);
+        }
+
+        private async Task SeedRoles(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            await roleManager.CreateAsync(new IdentityRole("SuperAdmin"));
+            await roleManager.CreateAsync(new IdentityRole("Admin"));
+            await roleManager.CreateAsync(new IdentityRole("User"));
+        }
+    }
+}
