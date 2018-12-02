c77ed9750052f8764ea5db710a8ae597c128bea3
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-02 17:44:39 +0200

    Changed project to .Net Standard Library.

---
+++ src/Data/Sportify.Data/SportifyDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿namespace Sportify.Data
+{
+    using System.IO;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Design;
+    using Microsoft.EntityFrameworkCore.Diagnostics;
+    using Microsoft.Extensions.Configuration;
+
+    public class SportifyDbContextFactory : IDesignTimeDbContextFactory<SportifyDbContext>
+    {
+        public SportifyDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<SportifyDbContext>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseSqlServer(connectionString);
+
+            builder.ConfigureWarnings(w => w.Throw(RelationalEventId.QueryClientEvaluationWarning));
+
+            return new SportifyDbContext(builder.Options);
+        }
+    }
+}
