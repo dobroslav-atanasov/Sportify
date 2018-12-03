0ddcb0750bfd192571cbeb6073e39aab4768bbe4
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-03 19:34:13 +0200

    Added new project Sportify.Data.ViewModels.

---
+++ src/Data/Sportify.Data.ViewModels/Countries/CountrySelectViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Sportify.Data.ViewModels.Countries
+{
+    using AutoMapping.Interfaces;
+    using Models;
+
+    public class CountrySelectViewModel : IMapFrom<Country>
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
