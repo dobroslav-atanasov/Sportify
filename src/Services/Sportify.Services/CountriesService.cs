7eac32040b545239f712295f0ebe093a9752f849
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-03 19:35:07 +0200

    Implemented CountriesService.cs.

---
+++ src/Services/Sportify.Services/CountriesService.cs
@@ -0,0 +1,48 @@
+﻿namespace Sportify.Services
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using AutoMapping;
+    using Data;
+    using Data.Models;
+    using Data.ViewModels.Countries;
+    using Interfaces;
+
+    public class CountriesService : ICountriesService
+    {
+        private readonly SportifyDbContext context;
+
+        public CountriesService(SportifyDbContext context)
+        {
+            this.context = context;
+        }
+
+        public Country GetCountryById(int id)
+        {
+            var country = this.context
+                .Countries
+                .FirstOrDefault(x => x.Id == id);
+
+            return country;
+        }
+
+        public Country GetCountryByName(string name)
+        {
+            var country = this.context
+                .Countries
+                .FirstOrDefault(x => x.Name == name);
+
+            return country;
+        }
+
+        public IEnumerable<CountrySelectViewModel> GetAllCountryNames()
+        {
+            var countries = this.context
+                .Countries
+                .OrderBy(x => x.Name)
+                .To<CountrySelectViewModel>();
+
+            return countries;
+        }
+    }
+}
---
+++ src/Services/Sportify.Services/Interfaces/ICountriesService.cs
@@ -0,0 +1,15 @@
+﻿namespace Sportify.Services.Interfaces
+{
+    using System.Collections.Generic;
+    using Data.Models;
+    using Data.ViewModels.Countries;
+
+    public interface ICountriesService
+    {
+        Country GetCountryById(int id);
+
+        Country GetCountryByName(string name);
+        
+        IEnumerable<CountrySelectViewModel> GetAllCountryNames();
+    }
+}
