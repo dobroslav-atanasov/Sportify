27b19ab1b8484dd62838d680de32893c1c1e3ecf
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-04 23:05:26 +0200

    Added new area - Administrator.

---
+++ src/Web/Sportify.Web/Areas/Administrator/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿namespace Sportify.Web.Areas.Administrator.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Area("Administrator")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
