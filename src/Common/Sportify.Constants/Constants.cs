d3afada69b0cfc22ddff5ceb61192a495212c78f
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-04 22:01:58 +0200

    Added new project - Constants. It will contain all constants in Sportify.

---
+++ src/Common/Sportify.Constants/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace Sportify.Constants
+{
+    public class Constants
+    {
+        public const string UsernameLengthErrorMessage = "The {0} must be at least {1} characters long!";
+
+        public const string UsernameInvalidSymbolsErrorMessage = "Username contains invalid symbols!";
+
+        public const string PasswordLengthErrorMessage = "The {0} must be at least {2} and at max {1} characters long.";
+
+        public const string ConfirmPasswordErrorMessage = "The password and confirmation password do not match.";
+    }
+}
