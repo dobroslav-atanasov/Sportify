d3c4d4a66e0f4631366071be16bf100bbefcb66e
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-04 19:30:56 +0200

    Added new migration AddColumnCountryCodeInCountry.

---
+++ src/Data/Sportify.Data/Migrations/20181204172519_AddColumnCountryCodeInCountry.cs
@@ -0,0 +1,52 @@
+﻿namespace Sportify.Data.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class AddColumnCountryCodeInCountry : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                "Density",
+                "Countries");
+
+            migrationBuilder.DropColumn(
+                "LandArea",
+                "Countries");
+
+            migrationBuilder.DropColumn(
+                "Population",
+                "Countries");
+
+            migrationBuilder.AddColumn<string>(
+                "CountryCode",
+                "Countries",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                "CountryCode",
+                "Countries");
+
+            migrationBuilder.AddColumn<int>(
+                "Density",
+                "Countries",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                "LandArea",
+                "Countries",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                "Population",
+                "Countries",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
