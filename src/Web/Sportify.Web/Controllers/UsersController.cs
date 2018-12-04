2a944252aa44e003beb2a96650e56640ee83834a
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-04 22:03:43 +0200

    Implemented CreateAccount method in UsersController.

--- src/Web/Sportify.Web/Controllers/UsersController.cs
+++ src/Web/Sportify.Web/Controllers/UsersController.cs
@@ -1,31 +1,40 @@
 ﻿namespace Sportify.Web.Controllers
 {
+    using Data.ViewModels.Users;
     using Microsoft.AspNetCore.Mvc;
-    using Models.Users;
     using Services.Interfaces;
 
     public class UsersController : Controller
     {
         private readonly ICountriesService countriesService;
+        private readonly IUsersService usersService;
 
-        public UsersController(ICountriesService countriesService)
+        public UsersController(ICountriesService countriesService, IUsersService usersService)
         {
             this.countriesService = countriesService;
+            this.usersService = usersService;
         }
 
-        public IActionResult Register()
+        public IActionResult CreateAccount()
         {
             this.ViewData["Countries"] = this.countriesService.GetAllCountryNames();
             return this.View();
         }
 
         [HttpPost]
-        public IActionResult Register(RegisterViewModel model)
+        public IActionResult CreateAccount(CreateAccountViewModel model)
         {
             if (!this.ModelState.IsValid)
             {
                 this.ViewData["Countries"] = this.countriesService.GetAllCountryNames();
                 return this.View(model);
+            }
+
+            var isRegister = this.usersService.CreateAccountAsync(model).GetAwaiter().GetResult();
+
+            if (!isRegister)
+            {
+                return this.View();
             }
 
             return this.RedirectToAction("Index", "Home");
