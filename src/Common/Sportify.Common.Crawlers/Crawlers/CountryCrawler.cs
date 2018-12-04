ce810011b7f7b9efcf8592786e1d617170f8b538
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-04 19:31:52 +0200

    Removed folder Extensions. Modified CountryCrawler.

--- src/Common/Sportify.Common.Crawlers/Crawlers/CountryCrawler.cs
+++ src/Common/Sportify.Common.Crawlers/Crawlers/CountryCrawler.cs
@@ -1,12 +1,10 @@
 ﻿namespace Sportify.Common.Crawlers.Crawlers
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
     using System.Text.RegularExpressions;
     using Data;
     using Data.Models;
-    using Extensions;
     using HtmlAgilityPack;
     using Interfaces;
     using Microsoft.Extensions.DependencyInjection;
@@ -14,7 +12,8 @@
 
     internal class CountryCrawler : ICrawler
     {
-        private const string MainCountryCrawlerUrl = "http://www.worldometers.info/geography/alphabetical-list-of-countries/";
+        //private const string MainCountryCrawlerUrl = "http://www.worldometers.info/geography/alphabetical-list-of-countries/";
+        private const string MainCountryCodeUrl = "https://en.wikipedia.org/wiki/List_of_IOC_country_codes";
 
         private readonly IServiceProvider serviceProvider;
         private readonly WebService webService;
@@ -29,59 +28,104 @@
         {
             var context = this.serviceProvider.GetService<SportifyDbContext>();
 
-            var html = this.webService.GetRequest(MainCountryCrawlerUrl);
+            //var html = this.webService.GetRequest(MainCountryCrawlerUrl);
+            //var document = new HtmlDocument();
+            //document.LoadHtml(html);
+
+            //var node = document.DocumentNode.SelectSingleNode("//table[@class='table table-hover table-condensed']");
+            //this.ExtractAllCountriesInfo(node.OuterHtml, context);
+
+            var html = this.webService.GetRequest(MainCountryCodeUrl);
             var document = new HtmlDocument();
             document.LoadHtml(html);
 
-            var node = document.DocumentNode.SelectSingleNode("//table[@class='table table-hover table-condensed']");
-            this.ExtractAllCountriesInfo(node.OuterHtml, context);
+            var node = document.DocumentNode.SelectSingleNode("//table[@class='wikitable sortable']");
+            this.ExtractCountries(node.OuterHtml, context);
         }
 
-        private void ExtractAllCountriesInfo(string nodeOuterHtml, SportifyDbContext context)
+        private void ExtractCountries(string outerHtml, SportifyDbContext context)
         {
             var document = new HtmlDocument();
-            document.LoadHtml(nodeOuterHtml);
+            document.LoadHtml(outerHtml);
 
-            var nodes = document.DocumentNode.SelectNodes("//tr").Skip(1);
-            foreach (var node in nodes)
+            var countryNodes = document.DocumentNode.SelectNodes("//tr").Skip(1);
+
+            foreach (var node in countryNodes)
             {
+                var country = new Country();
+
                 var doc = new HtmlDocument();
                 doc.LoadHtml(node.OuterHtml);
-
                 var cols = doc.DocumentNode.SelectNodes("//td");
-                var country = new Country();
-                for (int i = 0; i < 5; i++)
+                for (int i = 0; i < 2; i++)
                 {
+                    if (i == 0)
+                    {
+                        var code = this.ExtractData(cols[i].OuterHtml, "<span class=\"monospaced\">(.*?)</span>");
+                        country.CountryCode = code;
+                    }
                     if (i == 1)
                     {
-                        var countryName = this.ExtractData(cols[i].OuterHtml, "\">(.*?)<\\/td>");
-                        country.Name = countryName.UrlDecode();
-                    }
-
-                    if (i == 2)
-                    {
-                        var population = this.ExtractData(cols[i].OuterHtml, "\\/\">(.*?)<\\/a>");
-                        country.Population = int.Parse(population.Replace(",", ""));
-                    }
-
-                    if (i == 3)
-                    {
-                        var landArea = this.ExtractData(cols[i].OuterHtml, ">(.*?)</td>");
-                        country.LandArea = int.Parse(landArea.Replace(",", ""));
-                    }
-
-                    if (i == 4)
-                    {
-                        var density = this.ExtractData(cols[i].OuterHtml, ">(.*?)</td>");
-                        country.Density = int.Parse(density.Replace(",", ""));
+                        var name = this.ExtractData(cols[i].OuterHtml, "Olympics\">(.*?)</a>");
+                        if (name == "The Gambia")
+                        {
+                            name = "Gambia";
+                        }
+                        country.Name = name;
                     }
                 }
 
-                context.Add(country);
+                context.Countries.Add(country);
             }
 
             context.SaveChanges();
         }
+
+        //private void ExtractAllCountriesInfo(string nodeOuterHtml, SportifyDbContext context)
+        //{
+        //    var document = new HtmlDocument();
+        //    document.LoadHtml(nodeOuterHtml);
+
+        //    var nodes = document.DocumentNode.SelectNodes("//tr").Skip(1);
+        //    foreach (var node in nodes)
+        //    {
+        //        var doc = new HtmlDocument();
+        //        doc.LoadHtml(node.OuterHtml);
+
+        //        var cols = doc.DocumentNode.SelectNodes("//td");
+        //        var country = new Country();
+        //        for (int i = 0; i < 5; i++)
+        //        {
+        //            if (i == 1)
+        //            {
+        //                var countryName = this.ExtractData(cols[i].OuterHtml, "\">(.*?)<\\/td>");
+        //                country.Name = countryName.UrlDecode();
+        //            }
+
+        //            if (i == 2)
+        //            {
+        //                var population = this.ExtractData(cols[i].OuterHtml, "\\/\">(.*?)<\\/a>");
+        //                country.Population = int.Parse(population.Replace(",", ""));
+        //            }
+
+        //            if (i == 3)
+        //            {
+        //                var landArea = this.ExtractData(cols[i].OuterHtml, ">(.*?)</td>");
+        //                country.LandArea = int.Parse(landArea.Replace(",", ""));
+        //            }
+
+        //            if (i == 4)
+        //            {
+        //                var density = this.ExtractData(cols[i].OuterHtml, ">(.*?)</td>");
+        //                country.Density = int.Parse(density.Replace(",", ""));
+        //            }
+        //        }
+
+        //        context.Add(country);
+        //    }
+
+        //    context.SaveChanges();
+        //}
 
         private string ExtractData(string outerHtml, string pattern)
         {
