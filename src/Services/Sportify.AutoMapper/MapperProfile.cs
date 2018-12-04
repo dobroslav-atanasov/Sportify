232b4af3d4c8f7935e13565ee798a2edb78b1cda
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-04 21:39:27 +0200

    Added project AutoMapper.

---
+++ src/Services/Sportify.AutoMapper/MapperProfile.cs
@@ -0,0 +1,14 @@
+﻿namespace Sportify.AutoMapper
+{
+    using Data.Models;
+    using Data.ViewModels.Countries;
+    using global::AutoMapper;
+
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            this.CreateMap<Country, CountrySelectViewModel>().ReverseMap();
+        }
+    }
+}
