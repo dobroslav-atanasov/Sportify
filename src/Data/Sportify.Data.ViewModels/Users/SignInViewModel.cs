5938fec055e3d25aa3f3b2ca7cd0e95f0f148455
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-04 23:05:57 +0200

    Implemented view model - SignInViewModel.

---
+++ src/Data/Sportify.Data.ViewModels/Users/SignInViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Sportify.Data.ViewModels.Users
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class SignInViewModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
