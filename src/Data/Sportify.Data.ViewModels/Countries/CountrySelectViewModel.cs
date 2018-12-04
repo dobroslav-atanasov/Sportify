3a8d9585109200dc7c98d719f251ed022343c7dc
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-04 22:02:38 +0200

    Added CreateAccountViewModel.cs.

--- src/Data/Sportify.Data.ViewModels/Countries/CountrySelectViewModel.cs
+++ src/Data/Sportify.Data.ViewModels/Countries/CountrySelectViewModel.cs
@@ -1,9 +1,6 @@
 ﻿namespace Sportify.Data.ViewModels.Countries
 {
-    using AutoMapping.Interfaces;
-    using Models;
-
-    public class CountrySelectViewModel : IMapFrom<Country>
+    public class CountrySelectViewModel
     {
         public int Id { get; set; }
 
