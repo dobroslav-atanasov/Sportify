3939fb594f69fc463f226e7625cb111205ef3706
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-05 19:58:14 +0200

    Implemented view model - AddTownViewModel.cs

---
+++ src/Data/Sportify.Data.ViewModels/Towns/AddTownViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Sportify.Data.ViewModels.Towns
+{
+    using System.ComponentModel.DataAnnotations;
+    using Constants;
+
+    public class AddTownViewModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [RegularExpression(@"[a-zA-z\s]+", ErrorMessage = Constants.TownInvalidSymbolsErrorMessage)]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Country")]
+        public int CountryId { get; set; }
+    }
+}
