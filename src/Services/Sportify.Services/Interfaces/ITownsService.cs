3988ba8916350875cbeee8cc3beb17263a95d58e
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-06 23:22:27 +0200

    Modified TownsService.

--- src/Services/Sportify.Services/Interfaces/ITownsService.cs
+++ src/Services/Sportify.Services/Interfaces/ITownsService.cs
@@ -1,9 +1,16 @@
 ﻿namespace Sportify.Services.Interfaces
 {
+    using System.Collections.Generic;
     using Data.ViewModels.Towns;
 
     public interface ITownsService
     {
         void AddTown(AddTownViewModel model);
+
+        IEnumerable<TownViewModel> GetAllTowns();
+
+        TownViewModel GetTownById(int id);
+
+        bool IsDeleteTown(TownViewModel model);
     }
 }
--- src/Services/Sportify.Services/TownsService.cs
+++ src/Services/Sportify.Services/TownsService.cs
@@ -1,21 +1,22 @@
 ﻿namespace Sportify.Services
 {
+    using System.Collections.Generic;
+    using System.Linq;
     using Data;
     using Data.Models;
     using Data.ViewModels.Towns;
     using global::AutoMapper;
     using Interfaces;
+    using Microsoft.EntityFrameworkCore;
 
     public class TownsService : ITownsService
     {
         private readonly SportifyDbContext context;
-        private readonly ICountriesService countriesService;
         private readonly IMapper mapper;
 
-        public TownsService(SportifyDbContext context, ICountriesService countriesService, IMapper mapper)
+        public TownsService(SportifyDbContext context, IMapper mapper)
         {
             this.context = context;
-            this.countriesService = countriesService;
             this.mapper = mapper;
         }
 
@@ -25,6 +26,49 @@
 
             this.context.Towns.Add(town);
             this.context.SaveChanges();
+        }
+
+        public IEnumerable<TownViewModel> GetAllTowns()
+        {
+            var towns = this.context
+                .Towns
+                .Include(t => t.Country)
+                .AsQueryable();
+
+            var townViewModels = this.mapper.Map<IQueryable<Town>, IEnumerable<TownViewModel>>(towns);
+
+            return townViewModels;
+        }
+
+        public TownViewModel GetTownById(int id)
+        {
+            var town = this.context
+                .Towns
+                .Include(t => t.Country)
+                .FirstOrDefault(t => t.Id == id);
+
+            var townViewModel = this.mapper.Map<TownViewModel>(town);
+
+            return townViewModel;
+        }
+
+        public bool IsDeleteTown(TownViewModel model)
+        {
+            var town = this.context
+                .Towns
+                .Include(t => t.Country)
+                .FirstOrDefault(t => t.Id == model.Id);
+
+            if (town != null)
+            {
+                this.context.Towns.Remove(town);
+
+                this.context.SaveChanges();
+
+                return true;
+            }
+
+            return false;
         }
     }
 }
