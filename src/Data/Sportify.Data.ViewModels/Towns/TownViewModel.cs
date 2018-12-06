83104c2efa911428d37b8f3a3cd6b606303032b8
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-06 23:21:31 +0200

    Added view model - TownViewModel.

---
+++ src/Data/Sportify.Data.ViewModels/Towns/TownViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Sportify.Data.ViewModels.Towns
+{
+    public class TownViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int CountryId { get; set; }
+
+        public string CountryName { get; set; }
+    }
+}
