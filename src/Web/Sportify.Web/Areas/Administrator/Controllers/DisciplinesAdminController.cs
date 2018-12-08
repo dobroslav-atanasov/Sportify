ec359f0d45ecc7d08a39514cd879033b234c577d
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-08 20:21:03 +0200

    Added view - AllDisciplines.

--- src/Web/Sportify.Web/Areas/Administrator/Controllers/DisciplinesAdminController.cs
+++ src/Web/Sportify.Web/Areas/Administrator/Controllers/DisciplinesAdminController.cs
@@ -1,6 +1,29 @@
 ﻿namespace Sportify.Web.Areas.Administrator.Controllers
 {
-    public class DisciplinesAdminController
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Services.Interfaces;
+    using X.PagedList;
+
+    [Area("Administrator")]
+    public class DisciplinesAdminController : Controller
     {
+        private readonly IDisciplinesService disciplinesService;
+
+        public DisciplinesAdminController(IDisciplinesService disciplinesService)
+        {
+            this.disciplinesService = disciplinesService;
+        }
+        
+        [Authorize(Roles = "Administrator")]
+        public IActionResult AllDisciplines(int? page)
+        {
+            var disciplines = this.disciplinesService.GetAllDisciplines();
+
+            var pageNumber = page ?? 1;
+            var disciplinesOnPage = disciplines.ToPagedList(pageNumber, 10);
+
+            return this.View(disciplinesOnPage);
+        }
     }
 }
