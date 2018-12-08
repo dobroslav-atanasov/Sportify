3510a672524452113504cc3d85462827dc698faa
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-08 20:20:28 +0200

    Added tests for SportsService and DisciplineService.

---
+++ src/Tests/Sportify.Tests/DisciplinesServiceTests.cs
@@ -0,0 +1,35 @@
+﻿namespace Sportify.Tests
+{
+    using System.Linq;
+    using AutoMapper;
+    using Data;
+    using Data.Models;
+    using global::AutoMapper;
+    using Microsoft.EntityFrameworkCore;
+    using Services;
+    using Xunit;
+
+    public class DisciplinesServiceTests
+    {
+        [Fact]
+        public void GetAllUserssShouldReturnsCorrectCountUsingDbContext()
+        {
+            var options = new DbContextOptionsBuilder<SportifyDbContext>()
+                .UseInMemoryDatabase("Sportify_Database_Disciplines_1")
+                .Options;
+
+            var context = new SportifyDbContext(options);
+            var mapperConfig = new MapperConfiguration(m => m.AddProfile(new MapperProfile()));
+            var mapper = mapperConfig.CreateMapper();
+
+            context.Add(new Discipline());
+            context.Add(new Discipline());
+            context.SaveChanges();
+
+            var service = new DisciplinesService(context, mapper);
+
+            var count = service.GetAllDisciplines().Count();
+            Assert.Equal(2, count);
+        }
+    }
+}
---
+++ src/Tests/Sportify.Tests/SportsServiceTests.cs
@@ -0,0 +1,55 @@
+﻿namespace Sportify.Tests
+{
+    using System.Linq;
+    using AutoMapper;
+    using Data;
+    using Data.Models;
+    using Data.ViewModels.Sports;
+    using global::AutoMapper;
+    using Microsoft.EntityFrameworkCore;
+    using Services;
+    using Xunit;
+
+    public class SportsServiceTests
+    {
+        [Fact]
+        public void GetAllUserssShouldReturnsCorrectCountUsingDbContext()
+        {
+            var options = new DbContextOptionsBuilder<SportifyDbContext>()
+                .UseInMemoryDatabase("Sportify_Database_Sports_1")
+                .Options;
+
+            var context = new SportifyDbContext(options);
+            var mapperConfig = new MapperConfiguration(m => m.AddProfile(new MapperProfile()));
+            var mapper = mapperConfig.CreateMapper();
+
+            context.Add(new Sport());
+            context.Add(new Sport());
+            context.SaveChanges();
+
+            var service = new SportsService(context, mapper);
+
+            var count = service.GetAllSports().Count();
+            Assert.Equal(2, count);
+        }
+
+        [Fact]
+        public void AddSportShouldReturnsCorrectCountUsingDbContext()
+        {
+            var options = new DbContextOptionsBuilder<SportifyDbContext>()
+                .UseInMemoryDatabase("Sportify_Database_Sports_2")
+                .Options;
+
+            var context = new SportifyDbContext(options);
+            var mapperConfig = new MapperConfiguration(m => m.AddProfile(new MapperProfile()));
+            var mapper = mapperConfig.CreateMapper();
+
+            var service = new SportsService(context, mapper);
+            service.Add(new AddSportViewModel());
+
+            var count = context.Sports.Count();
+
+            Assert.Equal(1, count);
+        }
+    }
+}
