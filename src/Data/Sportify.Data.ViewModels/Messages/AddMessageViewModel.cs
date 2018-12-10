6d0c32ffed2463f12f1a6e808aa84fb29da75375
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-10 19:10:06 +0200

    Added new view model - AddMessageViewModel.

---
+++ src/Data/Sportify.Data.ViewModels/Messages/AddMessageViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Sportify.Data.ViewModels.Messages
+{
+    using System.ComponentModel.DataAnnotations;
+    using Constants;
+
+    public class AddMessageViewModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [MinLength(3, ErrorMessage = Constants.UsernameLengthErrorMessage)]
+        [RegularExpression("[a-zA-z0-9-.*/_]+", ErrorMessage = Constants.UsernameInvalidSymbolsErrorMessage)]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Content")]
+        public string Content { get; set; }
+    }
+}
