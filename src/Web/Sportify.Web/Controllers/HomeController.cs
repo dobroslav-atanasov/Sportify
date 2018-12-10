69ba38ea9ddaf421cff4270913c979c5fcb509c9
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-10 18:35:00 +0200

    Modified HomeController.

--- src/Web/Sportify.Web/Controllers/HomeController.cs
+++ src/Web/Sportify.Web/Controllers/HomeController.cs
@@ -1,9 +1,17 @@
 ﻿namespace Sportify.Web.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
+    using Services.Interfaces;
 
     public class HomeController : Controller
     {
+        private readonly ICountriesService countriesService;
+
+        public HomeController(ICountriesService countriesService)
+        {
+            this.countriesService = countriesService;
+        }
+
         public IActionResult Index()
         {
             if (this.User.IsInRole("Administrator"))
@@ -11,6 +19,12 @@
                 return this.RedirectToAction("Index", "HomeAdmin", new {area = "Administrator"});
             }
 
+            this.ViewData["Countries"] = this.countriesService.GetAllCountryNames();
+            return this.View();
+        }
+
+        public IActionResult AboutUs()
+        {
             return this.View();
         }
     }
