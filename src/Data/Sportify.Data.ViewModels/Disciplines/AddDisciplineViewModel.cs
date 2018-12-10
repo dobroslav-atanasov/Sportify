bb40bedd70083121d731f848e8bb53d2bd05f9e5
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-10 18:34:05 +0200

    Added new view model - AddDisciplineViewModel.

---
+++ src/Data/Sportify.Data.ViewModels/Disciplines/AddDisciplineViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Sportify.Data.ViewModels.Disciplines
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class AddDisciplineViewModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+
+        [Required]
+        [Display(Name = "Sport")]
+        public int SportId { get; set; }
+    }
+}
