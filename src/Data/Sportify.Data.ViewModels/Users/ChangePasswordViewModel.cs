6ccec6735e2ffd65d29fe02b6187294c9ab6a7b2
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-10 23:30:01 +0200

    Added new view model - ChangePasswordViewModel.

---
+++ src/Data/Sportify.Data.ViewModels/Users/ChangePasswordViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Sportify.Data.ViewModels.Users
+{
+    using System.ComponentModel.DataAnnotations;
+    using Constants;
+
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current password")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(10, ErrorMessage = Constants.PasswordLengthErrorMessage, MinimumLength = 4)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = Constants.ConfirmPasswordErrorMessage)]
+        [Display(Name = "Confirm new password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
