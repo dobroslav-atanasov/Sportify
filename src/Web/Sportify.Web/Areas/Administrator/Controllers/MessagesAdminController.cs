2595518d837482905c9d4d4fe5dfa33297f392bc
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-12-11 14:25:24 +0200

    Updated Sportify.Web project.

---
+++ src/Web/Sportify.Web/Areas/Administrator/Controllers/MessagesAdminController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Sportify.Services.Interfaces;
+using X.PagedList;
+
+namespace Sportify.Web.Areas.Administrator.Controllers
+{
+    [Area("Administrator")]
+    public class MessagesAdminController : Controller
+    {
+        private readonly IMessagesService messagesService;
+
+        public MessagesAdminController(IMessagesService messagesService)
+        {
+            this.messagesService = messagesService;
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public IActionResult AllMessages(int? page)
+        {
+            var users = this.messagesService.GetAllMessages();
+
+            var pageNumber = page ?? 1;
+            var usersOnPage = users.ToPagedList(pageNumber, 10);
+
+            return this.View(usersOnPage);
+        }
+    }
+}
