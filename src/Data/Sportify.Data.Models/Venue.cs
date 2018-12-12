bc09d6b1043a9f86b40e529ebbd9ddc7730b8383
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-12 19:52:50 +0200

    Added property Address to model Venue. Added new migration and updated database.

--- src/Data/Sportify.Data.Models/Venue.cs
+++ src/Data/Sportify.Data.Models/Venue.cs
@@ -13,6 +13,8 @@
 
         public string Name { get; set; }
 
+        public string Address { get; set; }
+
         public int Capacity { get; set; }
 
         public string ImageVenueUrl { get; set; }
