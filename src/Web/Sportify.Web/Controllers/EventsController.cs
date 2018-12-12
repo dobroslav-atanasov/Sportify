c85714e57df6f84eeb7198a05a76338fe944b9ef
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-12-12 11:52:11 +0200

    Updated web project.

---
+++ src/Web/Sportify.Web/Controllers/EventsController.cs
@@ -0,0 +1,14 @@
+﻿namespace Sportify.Web.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class EventsController : Controller
+    {
+        [Authorize(Roles = Constants.Constants.EditorRole)]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ src/Web/Sportify.Web/Controllers/VenuesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Sportify.Constants;
+
+namespace Sportify.Web.Controllers
+{
+    public class VenuesController : Controller
+    {
+        [Authorize(Roles = "Administrator, Editor")]
+        public IActionResult AddVenue()
+        {
+            return this.View();
+        }
+    }
+}
