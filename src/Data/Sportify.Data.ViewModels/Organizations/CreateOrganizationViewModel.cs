34577ddaafbb46441a3f260ef1a93abc059680b5
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-12-13 10:57:45 +0200

    Created view model - CreateOrganizationViewModel.

---
+++ src/Data/Sportify.Data.ViewModels/Organizations/CreateOrganizationViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sportify.Data.ViewModels.Organizations
+{
+    public class CreateOrganizationViewModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [MinLength(3, ErrorMessage = Constants.Constants.OrganizationNameLengthErrorMessage)]
+        [RegularExpression("[a-zA-z0-9-.*/_\\s]+", ErrorMessage = Constants.Constants.OrganizationNameInvalidSymbolsErrorMessage)]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [RegularExpression("[a-zA-z0-9-.*/_\\s]+", ErrorMessage = Constants.Constants.OrganizationDescriptionInvalidSymbolsErrorMessage)]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+    }
+}
