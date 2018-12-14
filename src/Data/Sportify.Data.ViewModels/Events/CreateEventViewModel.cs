ec252b5ca7dd1e53cd0f95ee0f581a48241197c2
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-12-14 14:22:10 +0200

    Updated CreateEventViewModel.

--- src/Data/Sportify.Data.ViewModels/Events/CreateEventViewModel.cs
+++ src/Data/Sportify.Data.ViewModels/Events/CreateEventViewModel.cs
@@ -8,7 +8,7 @@
     {
         [Required]
         [DataType(DataType.Text)]
-        [RegularExpression("[a-zA-z0-9-.*/_]+", ErrorMessage = Constants.EventNameInvalidSymbolsErrorMessage)]
+        [RegularExpression("[a-zA-z0-9-.*/_\\s]+", ErrorMessage = Constants.EventNameInvalidSymbolsErrorMessage)]
         [Display(Name = "Event Name")]
         public string EventName { get; set; }
         
