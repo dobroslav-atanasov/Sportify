2689b60c977e954a5abb2108153cf8873678c470
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-12-14 14:23:40 +0200

    Updated web project.

--- src/Web/Sportify.Web/Controllers/EventsController.cs
+++ src/Web/Sportify.Web/Controllers/EventsController.cs
@@ -3,18 +3,21 @@
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
     using Services.Interfaces;
+    using Sportify.Data.ViewModels.Events;
 
     public class EventsController : Controller
     {
         private readonly IOrganizationsService organizationsService;
         private readonly IDisciplinesService disciplinesService;
         private readonly IVenuesService venuesService;
+        private readonly IEventsService eventsService;
 
-        public EventsController(IOrganizationsService organizationsService, IDisciplinesService disciplinesService, IVenuesService venuesService)
+        public EventsController(IOrganizationsService organizationsService, IDisciplinesService disciplinesService, IVenuesService venuesService, IEventsService eventsService)
         {
             this.organizationsService = organizationsService;
             this.disciplinesService = disciplinesService;
             this.venuesService = venuesService;
+            this.eventsService = eventsService;
         }
 
         [Authorize(Roles = Constants.Constants.EditorRole)]
@@ -24,6 +27,19 @@
             this.ViewData["Disciplines"] = this.disciplinesService.GetAllDisciplines();
             this.ViewData["Venues"] = this.venuesService.GetAllVenues();
             return this.View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = Constants.Constants.EditorRole)]
+        public IActionResult Create(CreateEventViewModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            this.eventsService.Create(model);
+            return this.RedirectToAction("Index", "Home");
         }
     }
 }
