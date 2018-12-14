bf8a06eac645f96610e10182669a17418de1efd6
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-12-14 09:49:45 +0200

    Removed all methods which use UserManager and SignInManager.

--- src/Services/Sportify.Services/Interfaces/IUsersService.cs
+++ src/Services/Sportify.Services/Interfaces/IUsersService.cs
@@ -6,22 +6,8 @@
 
     public interface IUsersService
     {
-        Task<bool> CreateAccountAsync(CreateAccountViewModel model);
-
-        bool SignIn(SignInViewModel model);
-
-        void SignOut();
-
         IEnumerable<UserAdminViewModel> GetAllUsers();
 
-        ProfileUserViewModel GetCurrentUser(string username);
-
-        bool UpdateProfile(string username, ProfileUserViewModel model);
-
         bool IsUsernameExist(string username);
-
-        bool ChangePassword(string username, ChangePasswordViewModel model);
-
-        void ChangeRole(UserIdViewModel model);
     }
 }
--- src/Services/Sportify.Services/UsersService.cs
+++ src/Services/Sportify.Services/UsersService.cs
@@ -21,44 +21,6 @@
             this.countriesService = countriesService;
         }
 
-        public async Task<bool> CreateAccountAsync(CreateAccountViewModel model)
-        {
-            var country = this.countriesService.GetCountryById(model.CountryId);
-
-            var user = this.Mapper.Map<User>(model);
-
-            var result = this.UserManager.CreateAsync(user, model.Password).GetAwaiter().GetResult();
-
-            if (result.Succeeded)
-            {
-                if (this.UserManager.Users.Count() == 1)
-                {
-                    await this.UserManager.AddToRoleAsync(user, "Administrator");
-                }
-                else
-                {
-                    await this.UserManager.AddToRoleAsync(user, "User");
-                }
-
-                this.SignInManager.SignInAsync(user, false).GetAwaiter().GetResult();
-            }
-
-
-            return result.Succeeded;
-        }
-
-        public bool SignIn(SignInViewModel model)
-        {
-            var result = this.SignInManager.PasswordSignInAsync(model.Username, model.Password, true, true).GetAwaiter().GetResult();
-
-            return result.Succeeded;
-        }
-
-        public void SignOut()
-        {
-            this.SignInManager.SignOutAsync().Wait();
-        }
-
         public IEnumerable<UserAdminViewModel> GetAllUsers()
         {
             var users = this.Context
@@ -71,77 +33,9 @@
             return usersAdminViewModel;
         }
 
-        public ProfileUserViewModel GetCurrentUser(string username)
-        {
-            var user = this.UserManager.FindByNameAsync(username).GetAwaiter().GetResult();
-            var model = this.Mapper.Map<ProfileUserViewModel>(user);
-
-            return model;
-        }
-
-        public bool UpdateProfile(string username, ProfileUserViewModel model)
-        {
-            var user = this.UserManager.FindByNameAsync(username).GetAwaiter().GetResult();
-
-            if (this.IsUsernameExist(model.Username))
-            {
-                return false;
-            }
-
-            user.Email = model.Email;
-            user.PhoneNumber = model.PhoneNumber;
-            user.BirthDate = model.BirthDate;
-            user.CountryId = model.CountryId;
-            user.FirstName = model.FirstName;
-            user.LastName = model.LastName;
-
-            var isUpdatedUser = this.UserManager.UpdateAsync(user).GetAwaiter().GetResult();
-            if (!isUpdatedUser.Succeeded)
-            {
-                return false;
-            }
-
-            return true;
-        }
-
         public bool IsUsernameExist(string username)
         {
             return this.Context.Users.Any(u => u.UserName == username);
-        }
-
-        public bool ChangePassword(string username, ChangePasswordViewModel model)
-        {
-            var user = this.UserManager.FindByNameAsync(username).GetAwaiter().GetResult();
-            var result = this.UserManager.ChangePasswordAsync(user, model.OldPassword, model.NewPassword).GetAwaiter().GetResult();
-
-            if (!result.Succeeded)
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-        public void ChangeRole(UserIdViewModel model)
-        {
-            var user = this.UserManager.Users.FirstOrDefault(u => u.Id == model.UserId);
-
-            var role = this.UserManager.GetRolesAsync(user).GetAwaiter().GetResult();
-
-            if (role[0] == Constants.UserRole)
-            {
-                var userRole = this.Context.UserRoles.FirstOrDefault(ur => ur.UserId == user.Id);
-                this.Context.UserRoles.Remove(userRole);
-
-                this.UserManager.AddToRoleAsync(user, Constants.EditorRole).GetAwaiter().GetResult();
-            }
-            else if (role[0] == Constants.EditorRole)
-            {
-                var userRole = this.Context.UserRoles.FirstOrDefault(ur => ur.UserId == user.Id);
-                this.Context.UserRoles.Remove(userRole);
-
-                this.UserManager.AddToRoleAsync(user, Constants.UserRole).GetAwaiter().GetResult();
-            }
         }
     }
 }
