bc5a760d87af86bff2b6233be330ab73c9f7ccd4
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-15 13:15:16 +0200

    Created EventViewModel.

---
+++ src/Data/Sportify.Data.ViewModels/Events/EventViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Sportify.Data.ViewModels.Events
+{
+    public class EventViewModel
+    {
+        public int Id { get; set; }
+
+        public string EventName { get; set; }
+
+        public string Date { get; set; }
+
+        public string Organization { get; set; }
+
+        public string Discipline { get; set; }
+
+        public string Venue { get; set; }
+
+        public int NumberOfParticipants { get; set; }
+    }
+}
