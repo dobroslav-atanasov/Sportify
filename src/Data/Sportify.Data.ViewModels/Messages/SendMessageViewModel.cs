5d616c3e143ca189fd17ea86929b12599165538f
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-18 22:11:07 +0200

    Removed old AddMessageViewModel and created SendMessageViewModel.

---
+++ src/Data/Sportify.Data.ViewModels/Messages/SendMessageViewModel.cs
@@ -0,0 +1,33 @@
+﻿namespace Sportify.Data.ViewModels.Messages
+{
+    using System.ComponentModel.DataAnnotations;
+    using Constants;
+
+    public class SendMessageViewModel
+    {
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [MinLength(Constants.MinUsernameLength, ErrorMessage = Constants.MessageFullNameLength)]
+        [RegularExpression("[a-zA-z0-9-.*/_\\s]+", ErrorMessage = Constants.MessageFullNameContainsInvalidSymbols)]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Subject")]
+        public string Subject { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Content")]
+        public string Content { get; set; }
+    }
+}
