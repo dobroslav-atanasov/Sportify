11b9610b93937187f5e03bc83a5e6e2422a505ea
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-18 22:12:30 +0200

    Updated project Sportify.Web.

--- src/Web/Sportify.Web/Controllers/HomeController.cs
+++ src/Web/Sportify.Web/Controllers/HomeController.cs
@@ -1,7 +1,5 @@
 ﻿namespace Sportify.Web.Controllers
 {
-    using Constants;
-    using Data.ViewModels.Messages;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
     using Services.Interfaces;
@@ -36,26 +34,6 @@
         public IActionResult AboutUs()
         {
             return this.View();
-        }
-
-        public IActionResult ContactUs()
-        {
-            return this.View();
-        }
-
-        [HttpPost]
-        public IActionResult ContactUs(AddMessageViewModel model)
-        {
-            User user = null;
-            if (this.signInManager.IsSignedIn(this.User))
-            {
-                user = this.userManager.FindByNameAsync(this.User.Identity.Name).GetAwaiter().GetResult();
-            }
-
-            this.messagesService.SendMessage(model, user);
-
-            this.ViewData["Message"] = Constants.MessageIsSentSuccessfully;
-            return this.View(model);
         }
 
         public IActionResult Access()
---
+++ src/Web/Sportify.Web/Controllers/MessagesController.cs
@@ -0,0 +1,52 @@
+﻿namespace Sportify.Web.Controllers
+{
+    using Data.Models;
+    using Data.ViewModels.Messages;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Services.Interfaces;
+
+    public class MessagesController : Controller
+    {
+        private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
+        private readonly IMessagesService messagesService;
+
+        public MessagesController(UserManager<User> userManager, SignInManager<User> signInManager, IMessagesService messagesService)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.messagesService = messagesService;
+        }
+
+        public IActionResult Send()
+        {
+            if (this.signInManager.IsSignedIn(this.User))
+            {
+                var user = this.userManager.FindByNameAsync(this.User.Identity.Name).GetAwaiter().GetResult();
+                this.ViewData["Username"] = user.UserName;
+                this.ViewData["Email"] = user.Email;
+            }
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Send(SendMessageViewModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            User user = null;
+            if (this.signInManager.IsSignedIn(this.User))
+            {
+                user = this.userManager.FindByNameAsync(this.User.Identity.Name).GetAwaiter().GetResult();
+            }
+
+            this.messagesService.Send(model, user);
+
+            return this.View("Thankyou", "Messages");
+        }
+    }
+}
