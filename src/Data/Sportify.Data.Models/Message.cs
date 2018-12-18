fd0c09fd83c8b4f7f90b41e459104d038334e811
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-18 22:10:37 +0200

    Modified model of Message.

--- src/Data/Sportify.Data.Models/Message.cs
+++ src/Data/Sportify.Data.Models/Message.cs
@@ -9,7 +9,11 @@
         public string UserId { get; set; }
         public virtual User User { get; set; }
 
-        public string Name { get; set; }
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Subject { get; set; }
 
         public string Content { get; set; }
 
@@ -17,7 +21,7 @@
 
         public bool Equals(Message other)
         {
-            return this.Name == other.Name && this.Content == other.Content;
+            return this.FullName == other.FullName && this.Subject == other.Subject && this.Content == other.Content;
         }
     }
 }
