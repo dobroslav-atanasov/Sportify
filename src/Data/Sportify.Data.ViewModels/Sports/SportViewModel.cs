a972f653fe1ac05b99ae6de60f1902a0e49e084d
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-19 22:10:57 +0200

    Added new properties to DisciplineViewModel and SportViewModel.

--- src/Data/Sportify.Data.ViewModels/Sports/SportViewModel.cs
+++ src/Data/Sportify.Data.ViewModels/Sports/SportViewModel.cs
@@ -1,9 +1,16 @@
 ﻿namespace Sportify.Data.ViewModels.Sports
 {
+    using System.Collections.Generic;
+    using Disciplines;
+
     public class SportViewModel
     {
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageSportUrl { get; set; }
     }
 }
