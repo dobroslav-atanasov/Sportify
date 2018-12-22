abbc61beb25cbef064d4c07a168d04c30036ec49
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-22 09:20:47 +0200

    Modified SportViewModel and TownViewModel.

--- src/Data/Sportify.Data.ViewModels/Sports/SportViewModel.cs
+++ src/Data/Sportify.Data.ViewModels/Sports/SportViewModel.cs
@@ -1,15 +1,28 @@
 ﻿namespace Sportify.Data.ViewModels.Sports
 {
     using System;
+    using System.ComponentModel.DataAnnotations;
+    using Constants;
 
     public class SportViewModel : IEquatable<SportViewModel>
     {
         public int Id { get; set; }
 
+        [Required]
+        [DataType(DataType.Text)]
+        [MinLength(ModelConstants.MinSportNameLength, ErrorMessage = ModelConstants.SportNameLengthErrorMessage)]
+        [RegularExpression(ModelConstants.AddSport_Regex_Name, ErrorMessage = ModelConstants.SportNameInvalidSymbolsErrorMessage)]
+        [Display(Name = ModelConstants.AddSport_Display_Name)]
         public string Name { get; set; }
 
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = ModelConstants.AddSport_Display_Description)]
         public string Description { get; set; }
 
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = ModelConstants.AddSport_Display_ImageUrl)]
         public string ImageSportUrl { get; set; }
 
         public bool Equals(SportViewModel other)
--- src/Data/Sportify.Data.ViewModels/Towns/TownViewModel.cs
+++ src/Data/Sportify.Data.ViewModels/Towns/TownViewModel.cs
@@ -1,13 +1,28 @@
 ﻿namespace Sportify.Data.ViewModels.Towns
 {
-    public class TownViewModel
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using Constants;
+
+    public class TownViewModel : IEquatable<TownViewModel>
     {
         public int Id { get; set; }
 
+        [Required]
+        [DataType(DataType.Text)]
+        [MinLength(ModelConstants.MinTownNameLength, ErrorMessage = ModelConstants.TownNameLengthErrorMessage)]
+        [RegularExpression(ModelConstants.AddTown_Regex_Name, ErrorMessage = ModelConstants.TownInvalidSymbolsErrorMessage)]
+        [Display(Name = ModelConstants.AddTown_Display_Name)]
         public string Name { get; set; }
 
+        [Required]
+        [Display(Name = ModelConstants.AddTown_Display_Country)]
         public int CountryId { get; set; }
 
         public string CountryName { get; set; }
+        public bool Equals(TownViewModel other)
+        {
+            return this.Id == other.Id && this.Name == other.Name && this.CountryId == other.CountryId;
+        }
     }
 }
