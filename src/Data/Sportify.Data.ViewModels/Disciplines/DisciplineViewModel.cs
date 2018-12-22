4a00c203c85c6e548b0eaea371363c9b648da2da
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-22 15:45:14 +0200

    Modified DisciplineViewModel.

--- src/Data/Sportify.Data.ViewModels/Disciplines/DisciplineViewModel.cs
+++ src/Data/Sportify.Data.ViewModels/Disciplines/DisciplineViewModel.cs
@@ -1,11 +1,32 @@
 ﻿namespace Sportify.Data.ViewModels.Disciplines
 {
-    public class DisciplineViewModel
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using Constants;
+
+    public class DisciplineViewModel : IEquatable<DisciplineViewModel>
     {
         public int Id { get; set; }
 
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = ModelConstants.AddDiscipline_Display_Name)]
         public string Name { get; set; }
 
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = ModelConstants.AddDiscipline_Display_Description)]
         public string Description { get; set; }
+
+        [Required]
+        [Display(Name = ModelConstants.AddDiscipline_Display_Sport)]
+        public int SportId { get; set; }
+
+        public string Sport { get; set; }
+        public bool Equals(DisciplineViewModel other)
+        {
+            return this.Id == other.Id && this.Name == other.Name
+                    && this.Description == other.Description && this.SportId == other.SportId;
+        }
     }
 }
