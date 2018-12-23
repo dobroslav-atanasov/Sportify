0eed761464ef04d8c478cc17e72cdb20b19aeb9a
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-23 18:28:50 +0200

    Created new views in Venues folder.

--- src/Web/Sportify.Web/Controllers/VenuesController.cs
+++ src/Web/Sportify.Web/Controllers/VenuesController.cs
@@ -1,9 +1,11 @@
 ﻿namespace Sportify.Web.Controllers
 {
+    using Constants;
     using Data.ViewModels.Venues;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
     using Services.Interfaces;
+    using X.PagedList;
 
     public class VenuesController : Controller
     {
@@ -16,27 +18,84 @@
             this.townsService = townsService;
         }
 
-        [Authorize(Roles = "Administrator, Editor")]
-        public IActionResult AllVenues()
+        [Authorize(Roles = Role.AdministratorAndEditor)]
+        public IActionResult All(int? page)
         {
             var venues = this.venuesService.GetAllVenues();
-            return this.View(venues);
+
+            var pageNumber = page ?? 1;
+            var venuesOnPage = venues.ToPagedList(pageNumber, 10);
+
+            return this.View(venuesOnPage);
         }
 
-        [Authorize(Roles = "Administrator, Editor")]
-        public IActionResult AddVenue()
+        [Authorize(Roles = Role.AdministratorAndEditor)]
+        public IActionResult Add()
         {
-            this.ViewData["Towns"] = this.townsService.GetAllTowns();
+            this.ViewData[GlobalConstants.Towns] = this.townsService.GetAllTowns();
             return this.View();
         }
 
         [HttpPost]
-        [Authorize(Roles = "Administrator, Editor")]
-        public IActionResult AddVenue(AddVenueViewModel model)
+        [Authorize(Roles = Role.AdministratorAndEditor)]
+        public IActionResult Add(AddVenueViewModel model)
         {
-            this.ViewData["Towns"] = this.townsService.GetAllTowns();
+            this.ViewData[GlobalConstants.Towns] = this.townsService.GetAllTowns();
             this.venuesService.AddVenue(model);
-            return this.RedirectToAction("AllVenues", "Venues");
+            return this.RedirectToAction("All", "Venues", new { area = AreaConstants.Base });
+        }
+
+        [Authorize(Roles = Role.AdministratorAndEditor)]
+        public IActionResult Edit(int id)
+        {
+            this.ViewData[GlobalConstants.Towns] = this.townsService.GetAllTowns();
+            var venue = this.venuesService.GetVenueById(id);
+            return this.View(venue);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = Role.AdministratorAndEditor)]
+        public IActionResult Edit(VenueViewModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                this.ViewData[GlobalConstants.Towns] = this.townsService.GetAllTowns();
+                return this.View(model);
+            }
+
+            var venue = this.venuesService.UpdateVenue(model);
+            if (venue == null)
+            {
+                this.ViewData[GlobalConstants.Towns] = this.townsService.GetAllTowns();
+                this.ViewData[GlobalConstants.Error] = GlobalConstants.VenueWasNotUpdated;
+                return this.View(model);
+            }
+
+            this.ViewData[GlobalConstants.Message] = GlobalConstants.VenueWasUpdated;
+            this.ViewData[GlobalConstants.Towns] = this.townsService.GetAllTowns();
+            return this.View();
+        }
+
+        [Authorize(Roles = Role.AdministratorAndEditor)]
+        public IActionResult Details(int id)
+        {
+            var venue = this.venuesService.GetVenueById(id);
+            return this.View(venue);
+        }
+
+        [Authorize(Roles = Role.AdministratorAndEditor)]
+        public IActionResult Delete(int id)
+        {
+            var venue = this.venuesService.GetVenueById(id);
+            return this.View(venue);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = Role.Administrator)]
+        public IActionResult Delete(VenueViewModel model)
+        {
+            this.venuesService.DeleteVenue(model);
+            return this.RedirectToAction("All", "Venues", new { area = AreaConstants.Base });
         }
     }
 }
