8f59ee9625a9de7c5ce4fda2354fc810b59e7af5
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-26 09:33:59 +0200

    Implemented IEquatable interface and added attributes.

--- src/Data/Sportify.Data.ViewModels/Organizations/OrganizationViewModel.cs
+++ src/Data/Sportify.Data.ViewModels/Organizations/OrganizationViewModel.cs
@@ -1,13 +1,38 @@
 ﻿namespace Sportify.Data.ViewModels.Organizations
 {
-    public class OrganizationViewModel
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using Constants;
+
+    public class OrganizationViewModel : IEquatable<OrganizationViewModel>
     {
         public int Id { get; set; }
 
+        [Required]
+        [DataType(DataType.Text)]
+        [MinLength(ModelConstants.MinOrganizationAbbreviationLength, ErrorMessage = ModelConstants.OrganizationAbbreviationLengthErrorMessage)]
+        [RegularExpression(ModelConstants.CreateOrganization_Regex_Abbreviation, ErrorMessage = ModelConstants.OrganizationAbbreviationInvalidSymbolsErrorMessage)]
+        [Display(Name = ModelConstants.CreateOrganization_Display_Abbreviation)]
         public string Abbreviation { get; set; }
 
+        [Required]
+        [DataType(DataType.Text)]
+        [MinLength(ModelConstants.MinOrganizationNameLength, ErrorMessage = ModelConstants.OrganizationNameLengthErrorMessage)]
+        [RegularExpression(ModelConstants.CreateOrganization_Regex_Name, ErrorMessage = ModelConstants.OrganizationNameInvalidSymbolsErrorMessage)]
+        [Display(Name = ModelConstants.CreateOrganization_Display_Name)]
         public string Name { get; set; }
 
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = ModelConstants.CreateOrganization_Display_Description)]
+        public string Description { get; set; }
+
         public string President { get; set; }
+
+        public bool Equals(OrganizationViewModel other)
+        {
+            return this.Id == other.Id && this.Abbreviation == other.Abbreviation && this.Name == other.Name &&
+                   this.Description == other.Description;
+        }
     }
 }
