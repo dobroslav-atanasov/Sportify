6c6313e2698b7015b5a49657151313f7fc3aa0f7
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2018-12-31 10:24:56 +0200

    Created new view model - EventResultViewModel.

---
+++ src/Data/Sportify.Data.ViewModels/Results/EventResultViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Sportify.Data.ViewModels.Results
+{
+    public class EventResultViewModel
+    {
+        public string Username { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Country { get; set; }
+
+        public string Result { get; set; }
+    }
+}
